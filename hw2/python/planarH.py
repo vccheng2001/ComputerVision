7441ae122c8da38e3982ce23ae2adab52a24651d
Cheng <Vivian.Cheng@c02g41jpmd6t.fios-router.home>
2021-09-25 23:49:19 -0400

    compute homography matrix

--- hw2/python/planarH.py
+++ hw2/python/planarH.py
@@ -1,76 +1,116 @@
 import numpy as np
+from numpy.linalg.linalg import _matrix_rank_dispatcher
 import cv2
-
+import itertools
 
 def computeH(x1, x2):
-	#Q2.2.1
-	#Compute the homography between two sets of points
+    N, _ = x1.shape # N x 2
+    
+    row = 0
+    A = np.zeros((2*N, 9))
+    for i in range(0, len(x1)):
+        x, xp  = x1[i][0], x2[i][0]
+        y, yp = x1[i][1], x2[i][1]
+
+        A[row] = [x, y, 1, 0, 0, 0, -xp*x, -xp*y, -xp]
+        A[row+1] = [0, 0, 0, x, y, 1, -yp*x, -yp*y, -yp]
+
+        row += 2
 
 
+    U, S, VH = np.linalg.svd(A, full_matrices=True)
+
+    H2to1 = VH[8, :]
+    H2to1 = H2to1.reshape(3,3)
+    print('H2to1', H2to1)
+    return H2to1
 
 
-	return H2to1
-
+# x1 = np.random.rand(4,2)
+# x2 = np.random.rand(4,2)
+# computeH(x1,x2)
 
 def computeH_norm(x1, x2):
-	#Q2.2.2
-	#Compute the centroid of the points
+    # x1: nx2
+    #Q2.2.2
+    #Compute the centroid of the points
+
+    x1_centroid = np.mean(x1, axis=0)
+    x2_centroid = np.mean(x2, axis=0)
+
+    # Normalize: Shift the origin of the points to the centroid
+
+    x1 -= x1_centroid
+    x2 -= x2_centroid
 
 
-	#Shift the origin of the points to the centroid
+
+    #Normalize the points so that the largest distance from the origin is equal to sqrt(2)
+
+    max_dist_x1 = np.max(np.linalg.norm(x1-x1_centroid))
+    max_dist_x2 = np.max(np.linalg.norm(x2-x2_centroid))
+
+    print('max dists', max_dist_x1, max_dist_x2)
+
+    norm_factor_x1  = max_dist_x1 / np.sqrt(2)
+    norm_factor_x2  = max_dist_x2 / np.sqrt(2)
+
+    x1_norm = x1 / norm_factor_x1
+    x2_norm = x2 / norm_factor_x2
+
+    #Similarity transform 1
 
 
-	#Normalize the points so that the largest distance from the origin is equal to sqrt(2)
+    #Similarity transform 2
 
 
-	#Similarity transform 1
+    #Compute homography x2_tilde to x1_tilde
+    H2to1 = computeH(x1_norm, x2_norm)
+
+    print('H in computeH_norm', H)
+    #Denormalization
+    
+
+    return H2to1
 
 
-	#Similarity transform 2
 
-
-	#Compute homography
-
-
-	#Denormalization
-	
-
-	return H2to1
-
-
+x1 = np.random.rand(4,2)
+x2 = np.random.rand(4,2)
+computeH_norm(x1,x2)
 
 
 def computeH_ransac(locs1, locs2, opts):
-	#Q2.2.3
-	#Compute the best fitting homography given a list of matching points
-	max_iters = opts.max_iters  # the number of iterations to run RANSAC for
-	inlier_tol = opts.inlier_tol # the tolerance value for considering a point to be an inlier
+    #Q2.2.3
+    #Compute the best fitting homography given a list of matching points
+    max_iters = opts.max_iters  # the number of iterations to run RANSAC for
+    inlier_tol = opts.inlier_tol # the tolerance value for considering a point to be an inlier
 
-	
+    
 
 
-	return bestH2to1, inliers
+    return bestH2to1, inliers
 
 
 
 def compositeH(H2to1, template, img):
-	
-	#Create a composite image after warping the template image on top
-	#of the image using the homography
+    
+    #Create a composite image after warping the template image on top
+    #of the image using the homography
 
-	#Note that the homography we compute is from the image to the template;
-	#x_template = H2to1*x_photo
-	#For warping the template to the image, we need to invert it.
-	
+    #Note that the homography we compute is from the image to the template;
+    #x_template = H2to1*x_photo
+    #For warping the template to the image, we need to invert it.
+    
 
-	#Create mask of same size as template
+    #Create mask of same size as template
 
-	#Warp mask by appropriate homography
+    #Warp mask by appropriate homography
 
-	#Warp template by appropriate homography
+    #Warp template by appropriate homography
 
-	#Use mask to combine the warped template and the image
-	
-	return composite_img
+    #Use mask to combine the warped template and the image
+    
+    return composite_img
 
 
