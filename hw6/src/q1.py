d183d66d6d4968eb7d952dadf078b84ae181edbc
vccheng2001 <vccheng@andrew.cmu.edu>
2021-11-28 14:58:23 -0500

    q1c load hw6

---
+++ hw6/src/q1.py
@@ -0,0 +1,260 @@
+# ##################################################################### #
+# 16720: Computer Vision Homework 6
+# Carnegie Mellon University
+# April 20, 2020
+# ##################################################################### #
+
+# Imports
+import numpy as np
+from matplotlib import pyplot as plt
+from utils import integrateFrankot
+import scipy
+import scipy.io
+from PIL import Image
+
+def renderNDotLSphere(center, rad, light, pxSize, res):
+
+    """
+    Question 1 (b)
+
+    Render a sphere with a given center and radius. The camera is 
+    orthographic and looks towards the sphere in the negative z
+    direction. The camera's sensor axes are centerd on and aligned
+    with the x- and y-axes.
+
+    Parameters
+    ----------
+    center : numpy.ndarray
+        The center of the hemispherical bowl in an array of size (3,)
+
+    rad : float
+        The radius of the bowl
+
+    light : numpy.ndarray
+        The direction of incoming light
+
+    pxSize : float
+        Pixel size
+
+    res : numpy.ndarray
+        The resolution of the camera frame
+
+    Returns
+    -------
+    image : numpy.ndarray
+        The rendered image of the hemispherical bowl
+    """
+
+    ndotl = np.dot(n,light)
+    angle = np.arccos(ndotl)
+    image = None
+    return image
+
+
+##################################################################
+#                       1b 
+##################################################################
+# pxSize = 7e-6
+# res = np.array([3840,2160])
+# rad = 0.0075
+# center = np.array([0,0,10])
+# light1 = np.array([1,1,1]) / np.sqrt(3)
+# light2 = np.array([1,-1,1]) / np.sqrt(3)
+# light3 = np.array([-1,-1,1]) / np.sqrt(3)
+
+
+
+
+def loadData(path = "../data/"):
+
+    """
+    Question 1 (c)
+
+    Load data from the path given. The images are stored as input_n.tif
+    for n = {1...7}. The source lighting directions are stored in
+    sources.mat.
+
+    Paramters
+    ---------
+    path: str
+        Path of the data directory
+
+    Returns
+    -------
+    I : numpy.ndarray
+        The 7 x P matrix of vectorized images
+
+    L : numpy.ndarray
+        The 3 x 7 matrix of lighting directions
+
+    s: tuple
+        Image shape
+
+    """
+
+    im1 = np.array(Image.open(f'{path}input_1.tif')) 
+    s = (im1.shape[0], im1.shape[1])
+    I = im1.flatten()
+
+    # stack remaining imgs
+    for i in range(1,7):
+        im = np.array(Image.open(f'{path}input_{i+1}.tif'))
+        im = im.flatten()
+        I = np.vstack((I,im))
+
+    L = np.load(f'{path}/sources.npy').T # (3x7)
+    print('L', L.shape) # (3,7)
+    print('I', I.shape) # (7, 477117)
+    print('s', s)       # (431, 369)
+
+    return I, L, s
+
+
+
+##################################################################
+#                       1c
+##################################################################
+loadData()
+
+
+def estimatePseudonormalsCalibrated(I, L):
+
+    """
+    Question 1 (e)
+
+    In calibrated photometric stereo, estimate pseudonormals from the
+    light direction and image matrices
+
+    Parameters
+    ----------
+    I : numpy.ndarray
+        The 7 x P array of vectorized images
+
+    L : numpy.ndarray
+        The 3 x 7 array of lighting directions
+
+    Returns
+    -------
+    B : numpy.ndarray
+        The 3 x P matrix of pesudonormals
+    """
+
+    B = None
+    return B
+
+
+def estimateAlbedosNormals(B):
+
+    '''
+    Question 1 (e)
+
+    From the estimated pseudonormals, estimate the albedos and normals
+
+    Parameters
+    ----------
+    B : numpy.ndarray
+        The 3 x P matrix of estimated pseudonormals
+
+    Returns
+    -------
+    albedos : numpy.ndarray
+        The vector of albedos
+
+    normals : numpy.ndarray
+        The 3 x P matrix of normals
+    '''
+
+    albedos = None
+    normals = None
+    return albedos, normals
+
+
+def displayAlbedosNormals(albedos, normals, s):
+
+    """
+    Question 1 (f)
+
+    From the estimated pseudonormals, display the albedo and normal maps
+
+    Please make sure to use the `coolwarm` colormap for the albedo image
+    and the `rainbow` colormap for the normals.
+
+    Parameters
+    ----------
+    albedos : numpy.ndarray
+        The vector of albedos
+
+    normals : numpy.ndarray
+        The 3 x P matrix of normals
+
+    s : tuple
+        Image shape
+
+    Returns
+    -------
+    albedoIm : numpy.ndarray
+        Albedo image of shape s
+
+    normalIm : numpy.ndarray
+        Normals reshaped as an s x 3 image
+
+    """
+
+    albedoIm = None
+    normalIm = None
+
+    return albedoIm, normalIm
+
+
+def estimateShape(normals, s):
+
+    """
+    Question 1 (i)
+
+    Integrate the estimated normals to get an estimate of the depth map
+    of the surface.
+
+    Parameters
+    ----------
+    normals : numpy.ndarray
+        The 3 x P matrix of normals
+
+    s : tuple
+        Image shape
+
+    Returns
+    ----------
+    surface: numpy.ndarray
+        The image, of size s, of estimated depths at each point
+
+    """
+
+    surface = None
+    return surface
+
+
+def plotSurface(surface):
+
+    """
+    Question 1 (i) 
+
+    Plot the depth map as a surface
+
+    Parameters
+    ----------
+    surface : numpy.ndarray
+        The depth map to be plotted
+
+    Returns
+    -------
+        None
+
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+
+    # Put your main code here
+    pass
